6289e55a29c3d823ca643b1101e0f0c71903860b
Alan Bardales <alan.g.bardales@gmail.com>
2025-08-25 09:49:07 -0600

    feat: Add redirects for ads.txt in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel(),
+  redirects: {
+    '/ads.txt': 'https://srv.adstxtmanager.com/19390/alanbardales.dev'
+  }
 });
